70518824da4790493b1a7de5e2f9e3b0b8b992e3
zxeenu <zeenu.lastorder@gmail.com>
2025-09-14 14:33:39 +0500

    feat(api): add hono to api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!!!');
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
